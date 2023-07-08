bd81cef4c5a8f2d97ecc4929a207f867ad165dd3
reaganekoch12 <135785812+reaganekoch12@users.noreply.github.com>
2023-07-08 13:15:22 -0500

    taking out console.log on fetch

--- scriptHelper.js
+++ scriptHelper.js
@@ -57,7 +57,7 @@ async function addDestinationInfo(document, name, diameter, star, distance, moon
 async function myFetch(){
     const response = await fetch ('https://handlers.education.launchcode.org/static/planets.json');
     const print = await response. json();
-    console.log(print);
+   // console.log(print);
 }
   
 
